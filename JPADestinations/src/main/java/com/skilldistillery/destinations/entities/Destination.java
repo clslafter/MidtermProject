6db68921a2f969a042d7b865df50c75d743b679f
Aspen Newman <AspenNewman7@gmail.com>
2022-10-21 09:34:41 -0600

    Destination entity class created

---
+++ JPADestinations/src/main/java/com/skilldistillery/destinations/entities/Destination.java
@@ -0,0 +1,128 @@
+package com.skilldistillery.destinations.entities;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Destination {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+
+	private String description;
+
+	@Column(name = "website_url")
+	private String websiteUrl;
+
+	@Column(name = "create_date")
+	private LocalDateTime createDate;
+
+	@Column(name = "last_edited")
+	private LocalDateTime lastEdited;
+
+	@Column(name = "image_url")
+	private String imageUrl;
+
+	private boolean enabled;
+
+	public Destination() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getWebsiteUrl() {
+		return websiteUrl;
+	}
+
+	public void setWebsiteUrl(String websiteUrl) {
+		this.websiteUrl = websiteUrl;
+	}
+
+	public LocalDateTime getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(LocalDateTime createDate) {
+		this.createDate = createDate;
+	}
+
+	public LocalDateTime getLastEdited() {
+		return lastEdited;
+	}
+
+	public void setLastEdited(LocalDateTime lastEdited) {
+		this.lastEdited = lastEdited;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Destination other = (Destination) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Destination [id=" + id + ", name=" + name + ", description=" + description + ", websiteUrl="
+				+ websiteUrl + ", createDate=" + createDate + ", lastEdited=" + lastEdited + ", imageUrl=" + imageUrl
+				+ ", enabled=" + enabled + "]";
+	}
+
+}
