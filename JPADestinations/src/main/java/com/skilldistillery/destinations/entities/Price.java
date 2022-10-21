40b3e2e7693a080c57838140031d684caf3e91fa
Aspen Newman <AspenNewman7@gmail.com>
2022-10-21 09:46:48 -0600

    Price entity class created

---
+++ JPADestinations/src/main/java/com/skilldistillery/destinations/entities/Price.java
@@ -0,0 +1,69 @@
+package com.skilldistillery.destinations.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Price {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private Double amount;
+
+	private String description;
+
+	public Price() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Price other = (Price) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Price [id=" + id + ", amount=" + amount + ", description=" + description + "]";
+	}
+
+}
