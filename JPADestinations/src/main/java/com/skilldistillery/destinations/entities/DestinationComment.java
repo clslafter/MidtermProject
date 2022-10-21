43b3c88b1555fcf910d13aac898f7a55a8d7498d
Aspen Newman <AspenNewman7@gmail.com>
2022-10-21 09:39:29 -0600

    DestinationComment entity class created

---
+++ JPADestinations/src/main/java/com/skilldistillery/destinations/entities/DestinationComment.java
@@ -0,0 +1,86 @@
+package com.skilldistillery.destinations.entities;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "destination_comment")
+public class DestinationComment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String comment;
+
+	@Column(name = "created_date")
+	private LocalDateTime createdDate;
+
+	private boolean enabled;
+
+	public DestinationComment() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DestinationComment other = (DestinationComment) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "DestinationComment [id=" + id + ", comment=" + comment + ", createdDate=" + createdDate + ", enabled="
+				+ enabled + "]";
+	}
+
+}
