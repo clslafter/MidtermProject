c257dd1c843f258fb72b6cf4a5448cc67f42d6b2
Aspen Newman <AspenNewman7@gmail.com>
2022-10-21 18:15:43 -0600

    UserTest mapping passed

--- JPADestinations/src/test/java/com/skilldistillery/destinations/entities/UserTest.java
+++ JPADestinations/src/test/java/com/skilldistillery/destinations/entities/UserTest.java
@@ -2,6 +2,7 @@ package com.skilldistillery.destinations.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -42,11 +43,59 @@ class UserTest {
 		user = null;
 		
 	}
+	
 
 	@Test
 	void test_user_entity_mapping() {
 		assertNotNull(user);
 		assertEquals("admin", user.getUsername());
 	}
+	
+	@Test
+	void test_User_OTO_Address_association() {
+		assertNotNull(user);
+		assertEquals(1, user.getAddress().getId());
+		assertEquals("123 Main", user.getAddress().getStreet());
+		assertEquals("Parker", user.getAddress().getCity());
+	}
+	
+	@Test
+	void test_User_OTM_Destination_association() {
+		assertNotNull(user);
+		assertTrue(user.getDestinations().size() > 0);
+		
+	}
+	@Test
+	void test_User_OTM_DestinationImage_association() {
+		assertNotNull(user);
+		assertTrue(user.getImages().size() > 0);
+		
+	}
+	@Test
+	void test_User_OTM_DestinationComment_association() {
+		assertNotNull(user);
+		assertTrue(user.getDestinationComments().size() > 0);
+		
+	}
+	@Test
+	void test_User_OTM_ReviewComment_association() {
+		assertNotNull(user);
+		assertTrue(user.getReviewComments().size() > 0);
+		
+	}
+	@Test
+	void test_User_OTM_ReviewReaction_association() {
+		User user2 = em.find(User.class, 2);
+		assertNotNull(user2);
+		assertTrue(user2.getReviewReactions().size() > 0);
+		
+	}
+	@Test
+	void test_User_OTM_Review_association() {
+		assertNotNull(user);
+		assertTrue(user.getReviews().size() > 0);
+		
+	}
+	
 
 }
