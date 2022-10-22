981fb5c026bbc7b5f8dc217a5952b41951b43e0b
Aspen Newman <AspenNewman7@gmail.com>
2022-10-21 18:53:56 -0600

    ReviewComment relationship mapping passed

--- JPADestinations/src/test/java/com/skilldistillery/destinations/entities/ReviewCommentTest.java
+++ JPADestinations/src/test/java/com/skilldistillery/destinations/entities/ReviewCommentTest.java
@@ -47,5 +47,29 @@ class ReviewCommentTest {
 		assertNotNull(comment);
 		assertEquals("This is a Review Comment - Thanks for the review of the Grand Canyon.  I can't wait to go there.", comment.getComment());
 	}
-
+	
+	@Test
+	void test_ReviewComment_MTO_User_assocation(){
+		assertNotNull(comment);
+		assertEquals("Normal", comment.getUser().getFirstName());
+	}
+	
+	@Test
+	void test_ReviewComment_MTO_ReviewComment_assocation(){
+		ReviewComment comment2 = em.find(ReviewComment.class, 2);
+		assertNotNull(comment2);
+		assertEquals("This is a Review Comment - Thanks for the review of the Grand Canyon.  I can't wait to go there.", comment2.getReviewComment().getComment());
+	}
+	
+	@Test
+	void test_ReviewComment_OTM_ReviewComment_assocation(){
+		assertNotNull(comment);
+		assertTrue(comment.getReviewComments().size() > 0);
+	}
+	
+	@Test
+	void test_ReviewComment_MTO_Review_assocation(){
+		assertNotNull(comment);
+		assertEquals("This is a Review: The Grand Canyon is great!", comment.getReview().getComment());
+	}
 }
